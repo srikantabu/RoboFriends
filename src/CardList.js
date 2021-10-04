ba5b9c38002037601f03dc27b695139a49b8a539
Srikanta <busrikanta@gmail.com>
2021-10-04 20:25:05 +0530

    Some changes

---
+++ src/CardList.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Card from "./Card";
+
+let oPeople = [
+  {
+    id: 1,
+    name: "Srikanta B U",
+    email: "busrikanta@gmail.com",
+    img: "Srikanta",
+  },
+  {
+    id: 2,
+    name: "Umesha S",
+    email: "umeshas@gmail.com",
+    img: "Umesha",
+  },
+  {
+    id: 3,
+    name: "Arathi S",
+    email: "arathis@gmail.com",
+    img: "Arathi",
+  },
+  {
+    id: 4,
+    name: "Subhash B U",
+    email: "SubhashBU@gmail.com",
+    img: "Subhash",
+  },
+  {
+    id: 5,
+    name: "Swaroop Vel",
+    email: "SwaroopVel@gmail.com",
+    img: "Swaroop",
+  },
+];
+
+const CardList = () => {
+  // return(
+  //     {
+  //         oPeople.map((item) => (
+  //         <Card
+  //             id={item.id}
+  //             name={item.name}
+  //             email={item.email}
+  //             imglnk={item.img}
+  //         />
+  //     ))}
+  // );
+
+  return oPeople.map((item) => (
+    <Card id={item.id} name={item.name} email={item.email} imglnk={item.img} />
+  ));
+};
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Card from "./Card";
+// import Card from "./Card";
+import CardList from "./CardList";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
-let oPeople = [
-  {
-    id: 1,
-    name: "Srikanta B U",
-    email: "busrikanta@gmail.com",
-    img: "Srikanta",
-  },
-  {
-    id: 2,
-    name: "Umesha S",
-    email: "umeshas@gmail.com",
-    img: "Umesha",
-  },
-  {
-    id: 3,
-    name: "Arathi S",
-    email: "arathis@gmail.com",
-    img: "Arathi",
-  },
-  {
-    id: 4,
-    name: "Subhash B U",
-    email: "SubhashBU@gmail.com",
-    img: "Subhash",
-  },
-  {
-    id: 5,
-    name: "Swaroop Vel",
-    email: "SwaroopVel@gmail.com",
-    img: "Swaroop",
-  },
-];
-
 ReactDOM.render(
   <React.Fragment>
-    {oPeople.map((item) => (
-      <Card
-        id={item.id}
-        name={item.name}
-        email={item.email}
-        imglnk={item.img}
-      />
-    ))}
+    <CardList />
   </React.Fragment>,
 
   document.getElementById("root")
